b34036e33015c8276f9d84026515ca7a2d213659
unknown <Shane and Lianxin@.(none)>
2009-01-18 18:15:14 -0800

    recursive visitor

--- core/src/net/sf/cotta/AbstractFileVisitor.java
+++ core/src/net/sf/cotta/AbstractFileVisitor.java
@@ -1,10 +1,25 @@
 package net.sf.cotta;
 
 abstract public class AbstractFileVisitor implements FileVisitor {
+  private boolean recursive = true;
+
+  public AbstractFileVisitor() {
+    this(true);
+  }
+
+  public AbstractFileVisitor(boolean recursive) {
+    this.recursive = recursive;
+  }
+
   public void visit(TDirectory directory) throws TIoException {
-    TFile[] files = directory.listFiles();
-    for (int i = 0; i < files.length; i++) {
-      visit(files[i]);
+    for (TFile file : directory.listFiles()) {
+      visit(file);
+    }
+
+    if (recursive) {
+      for (TDirectory subDirectory : directory.listDirs()) {
+        subDirectory.visit(this);
+      }
     }
 
   }
